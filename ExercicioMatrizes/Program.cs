44f4a1e18ce4fc85e2c2b2d772ad89e32d60a6f6
Leonardo Gomes <leogms063@gmail.com>
2023-01-27 18:50:08 -0300

    Novas alterações

--- ExercicioMatrizes/Program.cs
+++ ExercicioMatrizes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Intrinsics.Arm;
 
 namespace ExercicioMatrizes
 {
@@ -6,7 +7,24 @@ namespace ExercicioMatrizes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+            Console.WriteLine("Quantas linhas e colunas você deseja para a Matriz? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine("Digite os dados da ");
         }
     }
 }
